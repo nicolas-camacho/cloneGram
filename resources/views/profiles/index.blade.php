5b796823135b5a2118c91a96285f54889480b057
nicolas-camacho <ncamacho1@cuc.edu.co>
2019-05-23 01:48:03 -0500

    upload post and counter succesfully

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="#">Add new post</a>
+                <a href="/p/create">Add new post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>189</strong> posts</div>
+            <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>233</strong> followers</div>
                 <div class="pr-5"><strong>1,495</strong> following</div>
             </div>
@@ -23,15 +23,11 @@
     </div>
 
     <div class="row pt-5">
+        @foreach ($user->posts as $post)
         <div class="col-4">
-            <img src="https://instagram.fbaq5-1.fna.fbcdn.net/vp/379720379f72d3c0721d5cc4d4d0ae79/5D9CDD9F/t51.2885-15/sh0.08/e35/s640x640/43028263_610471199368469_2573946356043251517_n.jpg?_nc_ht=instagram.fbaq5-1.fna.fbcdn.net" class="w-100">
+        <img src="/storage/{{ $post->image }}" class="w-100">
         </div>
-        <div class="col-4">
-            <img src="https://instagram.fbaq5-1.fna.fbcdn.net/vp/fc140390e98085cf67777218e401898d/5D63273F/t51.2885-15/sh0.08/e35/s640x640/42400953_909821129207432_5582143963570556964_n.jpg?_nc_ht=instagram.fbaq5-1.fna.fbcdn.net" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://instagram.fbaq5-1.fna.fbcdn.net/vp/a52f75642fc749fc32dd1f7474eb3142/5D690ABD/t51.2885-15/e35/59657639_417927138939803_4536913186304568694_n.jpg?_nc_ht=instagram.fbaq5-1.fna.fbcdn.net" class="w-100">
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
